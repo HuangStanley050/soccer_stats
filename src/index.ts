3afcf46e239d8d2a7e04b491d8243daabda740c7
Stanley Huang <huang.stanley050@gmail.com>
2019-10-11 10:13:07 +1100

    tuples

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { CsvFileReader } from "./CsvFileReader";
-
+import { MatchResult } from "./MatchResults";
 const reader = new CsvFileReader("football.csv");
 reader.read();
 
 let manUnitedWins = 0;
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D"
-}
 
 for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
